b1a1b9c67981d91f3b2c54414b7ce86689863358
Xander C. <swisshiss56@gmail.com>
2026-03-29 00:58:29 -0600

    menu half done

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -2,12 +2,35 @@
 
 void Game::initVars() {
     this->window = nullptr;
+
+    // So game starts on menu
+    this->state = GameState::Menu;
+}
+
+void Game::initFonts() {
+    this->font.openFromFile("assets/font.ttf");
+}
+
+void Game::initText() {
+    this->titleText.setFont(this->font);
+    this->titleText.setString("SNAKE");
+    this->titleText.setCharacterSize(60);
+    this->titleText.setFillColor(sf::Color::Green);
+    this->titleText.setPosition({200.f, 150.f});
+
+    this->startText.setFont(this->font);
+    this->startText.setString("Press Enter to Play");
+    this->startText.setCharacterSize(24);
+    this->startText.setFillColor(sf::Color::White);
+    this->startText.setPosition({170.f, 300.f});
 }
 
 void Game::initWindow() {
     this->window = new sf::RenderWindow(sf::VideoMode({600, 600}), "Snake Game", sf::Style::Titlebar | sf::Style::Close);
 }
 
+
+// Finish menu from this point downwards
 void Game::pollEvents() {
     while (const auto event = this->window->pollEvent()) {
         if (event->is<sf::Event::Closed>()) {
--- src/game/game.hpp
+++ src/game/game.hpp
@@ -4,6 +4,13 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Network.hpp>
 
+// Denotes what state the game is in
+enum class GameState {
+    Menu,
+    inGame,
+    GameOver
+};
+
 
 // Class that acts as the game engine
 class Game {
@@ -11,14 +18,35 @@ class Game {
         // Window
         sf::RenderWindow* window;
 
+        // Controls what state the game is in
+        GameState state;
+
         // Board - handles rendering for the board and updating the elements of the board
         Board b;
+
+        // Font var
+        sf::Font font;
+
+        // The title text
+        sf::Text titleText;
+
+        // Text that pops up to denote text
+        sf::Text startText;
+
+        // Text that pops up when game is over
+        sf::Text gameOverText;
 
         // Initialize variables
         void initVars();
 
         // Create Window for game
         void initWindow();
+
+        // Initilizes fonts
+        void initFonts();
+
+        // Initilizes texts
+        void initText();
 
         // Handle window/keyboard events
         void pollEvents();
